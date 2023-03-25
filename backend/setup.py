ae2205abd648c8863079b239d71ff92f49055e49
Ma_xidi <maxidi123@163.com>
2023-03-25 11:54:46 +0800

    MXD_init

--- backend/setup.py
+++ backend/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
 requirements=[
+
 ]
 setup(name='tra', version='0.1.1', packages=find_packages(), include_package_data=True, install_requires=requirements)
