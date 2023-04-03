96c63b29a0de78e9a6481657044cf0d0ef9ed318
Ruby Xia <rubixue572@gmail.com>
2023-04-03 16:44:37 +0800

    Dev xrb 景点搜索 (#6)
    * 调整calendar组件到components目录，为求统一
    
    * 添加工具, 批量rm pyc
    
    * 景点搜索

---
+++ frontend/wapp/pages/home/home.js
@@ -0,0 +1,69 @@
+// pages/home/home.js
+const utils = require("../../utils/util.js");
+
+Page({
+    data: {
+      server_imagename: utils.server_imagename
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad() {
+      this.setData({
+        opacity: 1,
+      })
+    },
+  
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */    
+    onShow() {
+      this.setData({
+        opacity: 1,
+      })
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+      this.setData({
+        opacity: 0,
+      })
+    },
+  
+    // 跳转到新页面
+    navigateToNewPage() {
+      wx.navigateTo({
+        url: '/pages/index/index',
+      })
+  
+      // 原页面淡出
+      this.setData({
+        opacity: 0,
+      })
+  
+      // 等待0.5秒，将原页面隐藏
+      setTimeout(() => {
+        this.setData({
+          opacity: 0,
+          hidden: true,
+        })
+      }, 500)
+    },
+
+    onSearchBarTap() {
+      wx.navigateTo({
+        url: '/pages/sceneSearch/sceneSearch',
+      })
+    }
+
+})
