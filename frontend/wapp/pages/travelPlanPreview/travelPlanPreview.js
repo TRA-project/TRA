69eea8545a627830c032a9fc2457af31294ed6b7
Elucidator-xrb <rubixue572@gmail.com>
2023-04-13 14:51:33 +0800

    [feat](出行计划生成预览): van-steps模拟游览点列表+添加展示数据

--- frontend/wapp/pages/travelPlanPreview/travelPlanPreview.js
+++ frontend/wapp/pages/travelPlanPreview/travelPlanPreview.js
@@ -1,4 +1,6 @@
 // pages/travelPlanPreview/travelPlanPreview.js
+const utils = require("../../utils/util.js");
+
 Page({
 
   /**
@@ -17,7 +19,65 @@ Page({
       }
     ],
 
-    tabsActive: 0,
+    plansActive: 0,
+    travelPlansList: [
+      [
+        {
+          id: 1,
+          name: "北京航空航天大学",
+          position: "北京市海淀区学院路37号",
+          image: "scenery-preview.png"
+        },
+        {
+          id: 2,
+          name: "肯德基（长城电脑大厦店）",
+          position: "北京市海淀区学院路38号长城电脑大厦",
+          image: "scenery-preview.png"
+        },
+        {
+          id: 3,
+          name: "麦当劳（花园北路餐厅）",
+          position: "北京市海淀区学院路甲38号",
+          image: "scenery-preview.png"
+        },
+      ],
+      [
+        {
+          id: 4,
+          name: "层岩巨渊",
+          position: "提瓦特北陆璃月南侧",
+          image: "scenery-preview.png"
+        },
+        {
+          id: 5,
+          name: "道成林",
+          position: "提瓦特央陆须弥东侧",
+          image: "scenery-preview.png"
+        },
+        {
+          id: 6,
+          name: "恒纳兰那",
+          position: "提瓦特央陆须弥北侧",
+          image: "scenery-preview.png"
+        },
+      ],
+    ],
+
+    stepsActive: 0,
+    steps: [
+      {
+        text: '行程1',
+        desc: '描述信息',
+      },
+      {
+        text: '行程2',
+        desc: '描述信息',
+      },
+      {
+        text: '行程3',
+        desc: '描述信息',
+      },
+    ],
   },
 
   /**
@@ -62,6 +122,12 @@ Page({
       title: `切换到方案 ${event.detail.name + 1 }`,
       icon: 'none',
     });
+  },
+
+  onStepClick(event) {
+    this.setData({
+      stepsActive: event.detail
+    })
   },
 
   /**
