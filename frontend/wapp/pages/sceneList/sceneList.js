b0a71d007ff7bb2e926e3e7facf278aa89a9e90a
Elucidator-xrb <rubixue572@gmail.com>
2023-04-13 18:00:23 +0800

    [feat](景点列表): 添加type筛选功能（剩下两个尚未） 1. 改用SceneryShowList显示，每次修改dropdownMenu触发更新 2. 完善一些前端内置数据

--- frontend/wapp/pages/sceneList/sceneList.js
+++ frontend/wapp/pages/sceneList/sceneList.js
@@ -11,8 +11,9 @@ Page({
 
     optionType: [
       { text: '全部类型', value: 0 },
-      { text: '亲自出行', value: 1 },
+      { text: '亲子出行', value: 1 },
       { text: '休闲娱乐', value: 2 },
+      { text: '曲径探幽', value: 3 },
     ],
     typeValue: 0,
     optionTime: [
@@ -36,45 +37,55 @@ Page({
         name: "回忆之丘",
         desc: "艾恩格朗特47层南侧野外迷宫",
         tags: ["休闲娱乐", "复活"],
+        type: 2,
         time: "3h",
         score: 5,
-        price: 0,
+        price: 10,
+        image: "preview.png",
       },
       {
         id: 2,
         name: "西边山脉",
         desc: "艾恩格朗特55层，吞食水晶的魔龙出没",
         tags: ["亲子出行", "锻造材料"],
+        type: 1,
         time: "8h",
         score: 4,
         price: 50,
+        image: "preview.png",
       },
       {
         id: 3,
-        name: "回忆之丘",
-        desc: "艾恩格朗特47层南侧野外迷宫",
-        tags: ["休闲娱乐", "复活"],
+        name: "森之屋",
+        desc: "艾恩格朗特22层南部高拉尔村外围",
+        tags: ["曲径探幽"],
+        type: 3,
         time: "3h",
         score: 5,
         price: 0,
+        image: "preview.png",
       },
       {
         id: 4,
-        name: "回忆之丘",
-        desc: "艾恩格朗特47层南侧野外迷宫",
-        tags: ["休闲娱乐", "复活"],
+        name: "塞尔穆布鲁克",
+        desc: "艾恩格朗特61层中心市区",
+        tags: ["休闲娱乐", "居家"],
+        type: 2,
         time: "3h",
         score: 5,
         price: 10,
+        image: "preview.png",
       },
       {
         id: 5,
-        name: "回忆之丘",
-        desc: "艾恩格朗特47层南侧野外迷宫",
-        tags: ["休闲娱乐", "复活"],
+        name: "莉兹贝特武器店",
+        desc: "艾恩格朗特48层主街区琳达司",
+        tags: ["休闲娱乐", "锻造"],
+        type: 2,
         time: "3h",
         score: 5,
         price: 20,
+        image: "preview.png",
       },
     ]
   },
@@ -109,6 +120,14 @@ Page({
             sceneryTotalList: res.data
           })
         }
+        this.setData({
+          sceneryShowList: this.data.sceneryTotalList
+        })
+        console.log(this.data.sceneryShowList)
+      },
+      fail: (res) => {
+        console.log("请求景点列表失败")
+        console.log(res)
       },
     })
   },
@@ -131,11 +150,6 @@ Page({
     wx.navigateBack()
   },
 
-  selectTagScenery() {
-
-
-  },
-
   addScenery() {
     console.log("add scenery")
   },
@@ -147,6 +161,36 @@ Page({
     wx.navigateTo({
       url: "/pages/SceneryShow/SceneryShow?scenery_id=" + scenery_id,
     })
+  },
+
+  selectSceneryFromTag() {// 目前仅匹配了type
+    if (this.data.typeValue === 0) {
+      this.setData({
+        sceneryShowList: this.data.sceneryTotalList
+      })
+      console.log("show all", this.data.sceneryShowList)
+      return  // 返回
+    }
+    var newSceneryShowList = []
+    for (var index in this.data.sceneryTotalList) {
+      var scene = this.data.sceneryTotalList[index]
+      if (scene.type === this.data.typeValue) { 
+        newSceneryShowList.push(scene)
+      }
+    }
+    this.setData({
+      sceneryShowList: newSceneryShowList
+    })
+    console.log("update showList:", this.data.sceneryShowList)
+  },
+
+  onChangeType(event) {
+    this.setData({
+      typeValue: event.detail
+    })
+    console.log("change type to", this.data.typeValue)
+    var that = this // 使用自定义函数时得用
+    that.selectSceneryFromTag()
   },
 
   /**
