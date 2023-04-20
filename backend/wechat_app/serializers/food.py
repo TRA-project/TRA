56447df9ede54dc94e5c44853c46a72a53d49442
suyang <2506806016@qq.com>
2023-04-20 20:16:29 +0800

    [fix](image): 修复Image的序列化问题

--- backend/wechat_app/serializers/food.py
+++ backend/wechat_app/serializers/food.py
@@ -1,12 +1,11 @@
 from rest_framework import serializers
 from utility.models import Food, Image
-from .images import ImageSerializer, TestSerializer
+from .images import ImageSerializer
+
 
 class FoodSerializer(serializers.ModelSerializer):
-    images = TestSerializer(many=True, required=False, allow_null=True)
+    images = ImageSerializer(many=True, required=False, allow_null=True)
 
     class Meta:
         model = Food
         exclude = []
-
-    
