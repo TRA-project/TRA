09a7ca058513a331f95bd35edf7cf89d27c851da
rain1732 <3044550844@qq.com>
2023-04-23 17:42:45 +0800

    [feat](plan) 基本完成了旅行计划的整体逻辑

--- backend/wechat_app/apis/plan.py
+++ backend/wechat_app/apis/plan.py
@@ -16,7 +16,7 @@ from utility.models.plan import Plan
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.mixins import RetrieveModelMixin, CreateModelMixin
+from rest_framework.mixins import RetrieveModelMixin, CreateModelMixin, DestroyModelMixin
 
 from wechat_app.serializers.sight import SightSerializer, SightPlanSerializer
 
@@ -25,16 +25,16 @@ def calculate(list, tag):
     hot_random = random.random()
     grade_random = random.random()
     distance_random = random.random()
-    #tag_random = random.random()
+    # tag_random = random.random()
     n = size(list)
     dict = {}
     seq = 0
     for i in list:
         # num = hot_random * i.get('hot') + tag_random * (i.get('tag') == tag) + distance_random
-        num = hot_random * i.get('hot')+grade_random*i.get('grade')
+        num = hot_random * i.get('hot') + grade_random * i.get('grade')
         dict[seq] = num
         seq = seq + 1
-    sys = sorted(dict.items(), key=lambda d: d[1], reverse=False)[0:10]
+    sys = sorted(dict.items(), key=lambda d: d[1], reverse=False)[0:5]
     better_sight = []
     for i, j in sys:
         better_sight.append(list[i])
@@ -47,11 +47,12 @@ def calculate(list, tag):
     return better_sight
 
 
-class PlanApis(GenericViewSet, CreateModelMixin, RetrieveModelMixin):
+class PlanApis(GenericViewSet, CreateModelMixin, RetrieveModelMixin, DestroyModelMixin):
     flag = 0
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
 
+    # lookup_field = "id"
     def get_serializer_class(self):
         if self.flag == 0:
             return PlanSerializer
@@ -74,17 +75,42 @@ class PlanApis(GenericViewSet, CreateModelMixin, RetrieveModelMixin):
     def retrieve(self, request, *args, **kwargs):
         owner_id = permission.user_check(request)
         if owner_id <= 0:
-            return error_response(Error.NOT_LOGIN, 'Please login.', status=status.HTTP_403_FORBIDDEN)
+            owner_id = 1
+            # return error_response(Error.NOT_LOGIN, 'Please login.', status=status.HTTP_403_FORBIDDEN)
+
         # 缺少了安全性检验
-        return super().retrieve(request, *args, **kwargs)
+        plan = Plan.objects.get(id=kwargs.get('pk'))
+        serializer = PlanSerializer(instance=plan)
+        list = serializer.data.get('sights')
+        sights_detail = []
+        for i in list:
+            sight = Sight.objects.get(id=i)
+            sight_serializer = SightPlanSerializer(instance=sight)
+            sights_detail.append(sight_serializer.data)
+        dict = serializer.data
+        dict['sights_detail'] = sights_detail
+        return Response(dict)
 
     def list(self, request, *args, **kwargs):
         owner_id = permission.user_check(request)
         if owner_id <= 0:
-            return error_response(Error.NOT_LOGIN, 'Please login.', status=status.HTTP_403_FORBIDDEN)
+            owner_id = 1
+            # return error_response(Error.NOT_LOGIN, 'Please login.', status=status.HTTP_403_FORBIDDEN)
         queryset = Plan.objects.filter(owner=owner_id)
         serializer = PlanSerializer(queryset, many=True)
-        return Response(serializer.data)
+        all_list = []
+        for i in serializer.data:
+            list = i.get('sights')
+            sights_name = []
+            for j in list:
+                sight = Sight.objects.get(id=j)
+                sight_serializer = SightSerializer(instance=sight)
+                name = sight_serializer.data.get('name')
+                sights_name.append(name)
+            dict = i
+            dict['sights_name'] = sights_name
+            all_list.append(dict)
+        return Response(all_list)
 
     @action(methods=['GET'], detail=False, url_path='new')
     def new(self, request):
@@ -112,10 +138,14 @@ class PlanApis(GenericViewSet, CreateModelMixin, RetrieveModelMixin):
 
     def create(self, request, *args, **kwargs):
         sights = request.POST.get('sights')
-        name = request.POST.get('name')
+        try:
+            name = request.POST.get('name')
+        except:
+            name = '旅行计划'
         owner_id = permission.user_check(request)
         if owner_id <= 0:
             owner_id = 1
+            # return error_response(Error.NOT_LOGIN, 'Please login.', status=status.HTTP_403_FORBIDDEN)
         data = {'owner': owner_id, 'name': name}
         serializer = PlanSerializer(data=data)
         serializer.is_valid(raise_exception=True)
@@ -124,4 +154,11 @@ class PlanApis(GenericViewSet, CreateModelMixin, RetrieveModelMixin):
         for i in test:
             data = Sight.objects.get(id=eval(i))
             plan.sights.add(data)
-        return error_response(Error.SUCCESS, status=status.HTTP_201_CREATED)
+        return Response(PlanSerializer(plan).data)
+
+    def destroy(self, request, *args, **kwargs):
+        owner_id = permission.user_check(request)
+        if owner_id <= 0:
+            owner_id = 1
+            # return error_response(Error.NOT_LOGIN, 'Please login.', status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(self, request, *args, **kwargs)
