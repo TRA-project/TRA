a771455a0097be25f564654b0b40d04c6071824a
Elucidator-xrb <rubixue572@gmail.com>
2023-04-24 18:44:23 +0800

    [fix](出行计划生成预览): 梳理代码，彻底区分tap、longpress、move对应触发事件

--- frontend/wapp/components/movableList/movableList.js
+++ frontend/wapp/components/movableList/movableList.js
@@ -29,6 +29,10 @@ Component({
     statusLongpress: false,  /* 是否在longpress状态，用于在touchend中过滤短按结束的情况 */
     moveId: 0,
     endY: 0,
+
+    // 检测手势
+    touchStartX: 0,
+    touchStartY: 0,
   },
 
   /**
@@ -47,14 +51,14 @@ Component({
       //console.log("draw list:", JSON.stringify(this.properties.tarList))
     },
 
-    activateMovable(event) {
+    activateMovable() {
       this.setData({
         moveDisabled: false
       })
       console.log("movable activate: now you can move item")
     },
 
-    deactiveMovable(event) {
+    deactiveMovable() {
       this.setData({
         moveDisabled: true
       })
@@ -63,8 +67,9 @@ Component({
 
     onLongPress(event) {
       console.log("[longpress]")
-      this.activateMovable()
       console.log(event)
+
+      this.activateMovable()
       const {
         currentTarget: {dataset}
       } = event 
@@ -81,8 +86,6 @@ Component({
     // 注意：只有位置在发生改变才会持续触发bindchange
     onMoving(event) {  
       /* moving时，调用间隔很短，是否应考虑优化 */
-      //console.log("[moving...]")
-      //console.log(event)
       const {
         detail,
         currentTarget: {dataset}
@@ -93,29 +96,41 @@ Component({
       })
     },
 
+    onTouchStart(event) {
+      console.log("[touch start]")
+      console.log(event)
+      // 记录初始位置
+      this.setData({
+        touchStartX: event.changedTouches[0].pageX,
+        touchStartY: event.changedTouches[0].pageY,
+      })
+    },
+
     onTouchEnd(event) {
       console.log("[touch end]")
-      if (!this.data.statusLongpress) { // 过滤非长按结束的情况
-        console.log("not after longpress, it's a tab -- view spot\n")
-        this.onTabViewScene(event)
+      console.log(event)
+      // 记录结束位置
+      const touchEndX = event.changedTouches[0].pageX
+      const touchEndY = event.changedTouches[0].pageY
+
+      // 检测到longpress动作 (longpress > tap)
+      if (this.data.statusLongpress) {
+        console.log("detect longpress")
+        this.endMoveAndReorderList()
         return
       }
-      // 确定为长按结束的情况
-      var newList = this.properties.tarList // 其实感觉没必要深拷贝
-      console.log("change endY to", this.data.endY)
-      newList[this.data.moveId].y = this.data.endY
-      newList.sort((a, b) => {return a.y - b.y})
-      //console.log("after sorting:", JSON.stringify(newList)) 
-      // 这里list的y值不对劲的原因是浅拷贝，导致之后的drawList修改也会影响newList
-      // console.log打印obj时，其实仅传入地址，所以缩略图时运行时的（对的），打开折叠所见值是当下的（错的）
-      this.setData({
-        tarList: newList, // 这里是浅拷贝（也可能不是，因为没引子组件）
-        statusLongpress: false
-      })
-      //console.log("cur tarList:", JSON.stringify(this.data.tarList))
-      this.drawList()
-      this.deactiveMovable()
-      //console.log("\n")
+
+      // 检测到tap动作
+      if (touchEndX === this.data.touchStartX &&
+          touchEndY === this.data.touchStartY ) {
+        console.log("detect tab: view scenery details")
+        this.viewSceneDetail(event)
+        return
+      }
+      
+      // default
+      console.log("meaningless event, ignored")
+      return
     },
 
     deleteListItem(event) {
@@ -130,13 +145,28 @@ Component({
     },
     
     // 权益之计，组件耦合度太太太高了
-    onTabViewScene(event) {
+    viewSceneDetail(event) {
       console.log("tabView:", event)
       var idx = event.currentTarget.dataset.moveid
       var planId = this.data.tarList[idx].id
       wx.navigateTo({
         url: "/pages/sceneryShow/sceneryShow?scenery_id=" + planId,
       })
+    },
+
+    endMoveAndReorderList() {
+      var newList = this.properties.tarList // 其实感觉没必要深拷贝
+      console.log("change endY to", this.data.endY)
+      newList[this.data.moveId].y = this.data.endY
+      newList.sort((a, b) => {return a.y - b.y})
+      //console.log("after sorting:", JSON.stringify(newList)) 
+      this.setData({
+        tarList: newList, // 这里是浅拷贝（也可能不是，因为没引子组件）
+        statusLongpress: false
+      })
+      //console.log("cur tarList:", JSON.stringify(this.data.tarList))
+      this.drawList()
+      this.deactiveMovable()
     }
   },
 
