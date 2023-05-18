611e8731f4952b3fc6122d613b7869e571397b2a
Elucidator-xrb <rubixue572@gmail.com>
2023-05-18 15:24:47 +0800

    [feat](出行计划预览): 实现刷新，其他tag存在画面更新bug

---
+++ frontend/wapp/pages/travelPlanPreview/defaultData.js
@@ -0,0 +1,47 @@
+
+const travelPlansList = [
+  [
+    {
+      id: 1,
+      name: "北京航空航天大学",
+      position: "北京市海淀区学院路37号",
+      image: "scenery-preview.png"
+    },
+    {
+      id: 2,
+      name: "肯德基（长城电脑大厦店）",
+      position: "北京市海淀区学院路38号长城电脑大厦",
+      image: "scenery-preview.png"
+    },
+    {
+      id: 3,
+      name: "麦当劳（花园北路餐厅）",
+      position: "北京市海淀区学院路甲38号",
+      image: "scenery-preview.png"
+    },
+  ],
+  [
+    {
+      id: 4,
+      name: "层岩巨渊",
+      position: "提瓦特北陆璃月南侧",
+      image: "scenery-preview.png"
+    },
+    {
+      id: 5,
+      name: "道成林",
+      position: "提瓦特央陆须弥东侧",
+      image: "scenery-preview.png"
+    },
+    {
+      id: 6,
+      name: "恒纳兰那",
+      position: "提瓦特央陆须弥北侧",
+      image: "scenery-preview.png"
+    },
+  ],
+]
+
+module.exports = {
+  travelPlansList
+}
